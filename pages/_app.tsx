b6edc42177511d63f4674897835c018742c63156
WagingBull <70824724+yugeorpheus@users.noreply.github.com>
2023-08-17 10:59:04 -0400

    dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,34 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import type { AppProps } from 'next/app';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import {
-  arbitrum,
-  goerli,
-  mainnet,
-  optimism,
-  polygon,
-  zora,
-} from 'wagmi/chains';
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from '@rainbow-me/rainbowkit';
+import type { AppProps } from 'next/app';
+import {
+  configureChains,
+  createConfig,
+  WagmiConfig,
+  useAccount,
+  useConnect,
+  useContractRead,
+  useContractWrite,
+  useNetwork,
+  useWaitForTransaction,
+} from 'wagmi';
+import { mainnet } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
+import { useState, useEffect } from 'react';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    zora,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
-  ],
+  [mainnet],
   [publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  appName: 'Cryptopunks Viewer Dapp',
+  projectId: '95833e24c29e3edede75a94f877130d1',
   chains,
 });
 
@@ -41,7 +42,15 @@ const wagmiConfig = createConfig({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider
+        theme={darkTheme({
+          ...darkTheme.accentColors.purple,
+          borderRadius: 'medium',
+          overlayBlur: 'small',
+        })}
+        modalSize="compact"
+        chains={chains}
+      >
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
