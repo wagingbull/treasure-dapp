efb758901dbee1bfd4037c1c961dbb53495bc816
WagingBull <70824724+yugeorpheus@users.noreply.github.com>
2023-08-18 12:22:28 -0400

    fallback data

--- pages/api/inventory/[address].tsx
+++ pages/api/inventory/[address].tsx
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import Cors from 'cors';
 import client from '../../../lib/client';
+import fallbackData from './fallbackData.json';
 
 const cors = Cors({
   methods: ['POST', 'GET', 'HEAD'],
@@ -32,18 +33,21 @@ export default async function handler(
     await runMiddleware(req, res, cors);
     console.log('Received request with address:', req.query.address);
 
+    // Check if req.query.address is a valid string
     const address =
       typeof req.query.address === 'string' ? req.query.address : '';
 
-    const data = await client.getInventory({ address });
+    // Construct the full URL
+    const apiUrl = `https://${req.headers.host}/api/inventory/${address}`;
+
+    // Fetch data from the GraphQL client
+    const data = await client.getInventory({ address: apiUrl });
     console.log('Data fetched from API:', data);
 
     res.status(200).json(data);
   } catch (error) {
     console.error('Error fetching inventory:', error);
 
-    res
-      .status(500)
-      .json({ error: 'An error occurred while fetching inventory' });
+    res.status(500).json(fallbackData);
   }
 }
