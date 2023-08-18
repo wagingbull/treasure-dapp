d2d92841269bf0b556bc3dbdf76d610d5db4e7e4
WagingBull <70824724+yugeorpheus@users.noreply.github.com>
2023-08-18 12:07:51 -0400

    add cors package

--- pages/api/inventory/[address].tsx
+++ pages/api/inventory/[address].tsx
@@ -1,18 +1,41 @@
-import { GetInventoryQuery } from '../../../generated/app.generated';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Cors from 'cors';
 import client from '../../../lib/client';
 
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 export default async function handler(
-  req: { query: { address: any } },
-  res: {
-    status: (code: number) => any;
-    json: (data: GetInventoryQuery | { error: string }) => any;
-  }
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
   try {
+    await runMiddleware(req, res, cors);
     console.log('Received request with address:', req.query.address);
 
-    const data = await client.getInventory({ address: req.query.address });
+    const address =
+      typeof req.query.address === 'string' ? req.query.address : '';
 
+    const data = await client.getInventory({ address });
     console.log('Data fetched from API:', data);
 
     res.status(200).json(data);
