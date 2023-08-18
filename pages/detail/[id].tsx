4d4d805fa417a301fd1afca0144ec0a140a77f6a
WagingBull <70824724+yugeorpheus@users.noreply.github.com>
2023-08-18 07:00:08 -0400

    render punk attributes on detail page

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -9,32 +9,69 @@ interface DetailPageProps {
 
 const DetailPage: NextPage<DetailPageProps> = () => {
   const router = useRouter();
-  const detailId = Number(router.query.id) as number;
+  const detailId = Number(router.query.id);
   const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
 
-  const { data, isError, isLoading } = useContractRead({
+  const {
+    data: svgData,
+    isError: isSvgError,
+    isLoading: isSvgLoading,
+  } = useContractRead({
     address: `0x${contractAddress}`,
     abi: cryptopunksData,
     functionName: 'punkImageSvg',
     args: [detailId],
   });
 
+  const {
+    data: attributeData,
+    isError: isAttributeError,
+    isLoading: isAttributeLoading,
+  } = useContractRead({
+    address: `0x${contractAddress}`,
+    abi: cryptopunksData,
+    functionName: 'punkAttributes',
+    args: [detailId],
+  });
+
+  const attributeArray: string[] = (attributeData as string)?.split(',');
+  const renderedAttributes = attributeArray?.map((value, index) => (
+    <div key={index}>{value}</div>
+  ));
+
   const svgHtml =
-    typeof data === 'string'
-      ? data.replace('data:image/svg+xml;utf8,', '')
+    typeof svgData === 'string'
+      ? svgData.replace('data:image/svg+xml;utf8,', '')
       : '';
 
   return (
     <div>
-      {isLoading && <div>Loading...</div>}
-      {isError && <div>Error loading data for Cryptopunk ID: {detailId}</div>}
-      {svgHtml ? (
-        <div
-          style={{ width: 250 }}
-          dangerouslySetInnerHTML={{ __html: svgHtml }}
-        />
+      {isSvgLoading || isAttributeLoading ? (
+        <div>Loading cryptopunks data...</div>
       ) : (
-        <div>No SVG data available</div>
+        <>
+          {isSvgError && (
+            <div>Error loading SVG data for Cryptopunk ID: {detailId}</div>
+          )}
+          {isAttributeError && (
+            <div>
+              Error loading attribute data for Cryptopunk ID: {detailId}
+            </div>
+          )}
+          {svgHtml ? (
+            <div
+              style={{ width: 250 }}
+              dangerouslySetInnerHTML={{ __html: svgHtml }}
+            />
+          ) : (
+            ''
+          )}
+
+          <div>
+            <h2>Attributes:</h2>
+            {renderedAttributes}
+          </div>
+        </>
       )}
     </div>
   );
