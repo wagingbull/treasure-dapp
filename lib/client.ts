70132dea6242dc13011c05e545bf69fa6d52094c
WagingBull <70824724+yugeorpheus@users.noreply.github.com>
2023-08-18 13:32:24 -0400

    test full client url

--- lib/client.ts
+++ lib/client.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "../generated/app.generated";
 
-const client = getSdk(new GraphQLClient(process.env.NEXT_PUBLIC_CRYPTOPUNKS_API_URL!));
+const client = getSdk(new GraphQLClient('https://gateway.thegraph.com/api/2b11ac00415c862cdedc3a3b95852e37/subgraphs/id/YqMJatbgbqy1GodtbYZv4U9NzyaScCgSF7CAE5ivAM7'));
 
 export default client;
